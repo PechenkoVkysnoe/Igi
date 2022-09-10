40a129f312c885fd35342f8643267f41ad9d0235
PechenkoVkysnoe <1mishagrigorchuk1@mail.ru>
2022-09-10 17:47:02 +0300

    First lab

---
+++ WEB_053505_HRIGORCHUK/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WEB_053505_HRIGORCHUK.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
