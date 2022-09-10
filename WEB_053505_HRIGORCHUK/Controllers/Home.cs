72583f564cca3fa9182701db5624ec3860d2d6c9
PechenkoVkysnoe <1mishagrigorchuk1@mail.ru>
2022-09-10 22:26:56 +0300

    Second lab

---
+++ WEB_053505_HRIGORCHUK/Controllers/Home.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WEB_053505_HRIGORCHUK.Controllers
+{
+    
+    public class Home : Controller
+    {
+        
+    }
+}
--- WEB_053505_HRIGORCHUK/Controllers/HomeController.cs
+++ WEB_053505_HRIGORCHUK/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,15 +14,32 @@ namespace WEB_053505_HRIGORCHUK.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
+        private List<ListDemo> _listDemo;
+        public HomeController()
+        {
+            
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public IActionResult Privacy()
         {
@@ -33,5 +51,10 @@ namespace WEB_053505_HRIGORCHUK.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
     }
 }
